354d9000affca4c37bf63601665f8b605d0272f6
TemeASD <temesus@hotmail.com>
2018-10-16 19:18:44 +0300

    lisätty default react frontend joka ei vielä tee mitään

--- userservice/router/router.js
+++ userservice/router/router.js
@@ -1,26 +1,26 @@
 const express = require('express'),
-  app = express(),
-  passport = require('passport'),
-  passportService = require('../config/passport'),
-  AuthController = require('../controllers/authcontroller'),
-  UserController = require('../controllers/usercontroller')
+	app = express(),
+	passport = require('passport'),
+	passportService = require('../config/passport'),
+	AuthController = require('../controllers/authcontroller'),
+	UserController = require('../controllers/usercontroller')
 
 module.exports = (app) => {
-  
-  const router = express.Router();
-  app.use('/api', router);
 
-  const jwtAuth = passport.authenticate('jwt', { session: false });
+	const router = express.Router();
+	app.use('/api', router);
 
-  //testidata 
-  router.get('/user', UserController.getData);
-  
-  // Autentikaatioreitit
-  router.post('/auth/register', AuthController.localRegister);
-  router.post('/auth/login', AuthController.localLogin);
+	const jwtAuth = passport.authenticate('jwt', { session: false });
 
-  // Käyttäjä CRUD reitit
-  router.get('/user/:id', jwtAuth, UserController.getUser);
-  router.patch('/user/:id', jwtAuth, UserController.updateUser);
-  router.delete('/user/:id', jwtAuth, UserController.deleteUser);
+	//testidata 
+	router.get('/user', UserController.getData);
+
+	// Autentikaatioreitit
+	router.post('/auth/register', AuthController.localRegister);
+	router.post('/auth/login', AuthController.localLogin);
+
+	// Käyttäjä CRUD reitit
+	router.get('/user/:id', jwtAuth, UserController.getUser);
+	router.patch('/user/:id', jwtAuth, UserController.updateUser);
+	router.delete('/user/:id', jwtAuth, UserController.deleteUser);
 }
