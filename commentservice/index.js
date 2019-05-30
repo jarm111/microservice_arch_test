3a4eb451912c1fa8d1172541f9614772b7c3f699
Jarmo Syvälahti <44726773+jarm111@users.noreply.github.com>
2019-05-30 16:08:42 +0300

    Create post comment route

--- commentservice/index.js
+++ commentservice/index.js
@@ -1,11 +1,18 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const	bodyParser = require('body-parser');
+const	cors = require('cors');
 var mongoose = require('mongoose');
 const {port, dbUri} = require('./config/config');
+const router = require('./routers/commentRouter');
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use('/api', router);
+app.get('/', (req, res) => res.send('Comment Service api'));
 
 mongoose.connect(dbUri, {useNewUrlParser: true});
-
-app.get('/', (req, res) => res.send('Comment Service api'));
 
 app.listen(port, () => console.log(`Comment Service listening on port ${port}!`));
